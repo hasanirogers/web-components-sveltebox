926fde849d55e9efefb12bcf1726ceda7db803b5
hasanirogers <dev@hasanirogers.me>
2024-11-16 18:10:40 -0500

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Sveltebox
+
+Sveltebox is an integration repo demostrating how to use [Kemet UI Web Components](https://kemet.dev) with Svelte.
---
+++ src/lib/Counter.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+  let count: number = $state(0)
+  const increment = () => {
+    count += 1
+  }
+</script>
+
+<kemet-button onclick={increment}>
+  count is {count}
+</kemet-button>
